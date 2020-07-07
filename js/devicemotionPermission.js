a7b663400684990c353dd14c9db80962bc27b838
Saetbyeol Kwon <43006719+SaetbyeolKwon@users.noreply.github.com>
2020-07-07 16:22:57 -0400

    gyro

--- js/devicemotionPermission.js
+++ js/devicemotionPermission.js
@@ -1,11 +1,4 @@
 DeviceMotionEvent.requestPermission()
-    .then(permissionSatete => {
-        if(permissionState === 'granted') {
-            (t.addEventListener("devicemotion",o,!1),u.motion.active=!0)
-        }
-    })
-
-DeviceMotionEvent.requestPermission()
 .then(response => {
   if (response == 'granted') {
     window.addEventListener('devicemotion', (e) => {
