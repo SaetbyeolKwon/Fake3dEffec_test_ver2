50004414de698042adf6108e5f25122c92bb2177
Saetbyeol Kwon <43006719+SaetbyeolKwon@users.noreply.github.com>
2020-07-07 19:36:21 -0400

    index7 test

--- js/devicemotionPermission.js
+++ js/devicemotionPermission.js
@@ -70,22 +70,3 @@ function onClick() {
     }
   }
 onClick();
-
-// function onClick() {
-//     // feature detect
-//     if (typeof DeviceOrientationEvent.requestPermission === 'function') {
-//       DeviceOrientationEvent.requestPermission()
-//         .then(permissionState => {
-//           if (permissionState === 'granted') {
-//             console.log("OTpermission granted");
-//             window.addEventListener('deviceorientation', (e) => {
-//             });
-//           }
-//         })
-//         .catch(console.error);
-//     } else {
-//       // handle regular non iOS 13+ devices
-//     }
-//   }
-
-//   onClick();
